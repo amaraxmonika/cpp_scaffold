7136978f1231aa2f2c25c351ba2cf1db419ead1a
Chasen Johnson <chasenjohnson@email.arizona.edu>
2018-03-15 23:58:13 -0700

    adding function to check version with -h flag

---
+++ include/common.h
@@ -0,0 +1,33 @@
+#include <iostream>
+
+class Util {
+    public:
+        static bool checkVersion(int argc, char** argv) {
+            // check no args
+            if(argc < 2) {
+                return false;
+            }
+
+            // argc at least 2
+            const char* shortV = "-v";
+            const char* longV = "--version";
+            int argLength = strlen(argv[1]);
+            bool isShortVersion = strcmp(shortV, argv[1]);
+            bool isLongVersion = strcmp(longV, argv[1]);
+            bool hasVersion = (isShortVersion || isLongVersion);
+            bool correctLength = (argLength == strlen(shortV)) || (argLength == strlen(longV));
+            if (hasVersion && !correctLength) {
+                std::cout << "Did you intend to check the version...?" << std::endl;
+                return true;
+            }
+            else if (hasVersion) {
+                // print version
+                std::cout << MAJOR_VERSION << "." <<
+                    MINOR_VERSION << "-" <<
+                    MINOR_REVISION << std::endl;
+                return true;
+            }
+
+            return false;
+    }
+};
--- src/test.cpp
+++ src/test.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include "test.h"
+#include "common.h"
 
 int main(int argc, char **argv) {
 
+    Util::checkVersion(argc, argv);
     std::cout << "this works" << std::endl;
     return 0;
 }
